fc86a76ab815a9dc6d473dc489687a2134d24317
Abhijeet Chawan <aryanachawan@gmail.com>
2021-08-12 18:28:19 -0500

    alt names for images

--- src/containers/Pages/Food_page/index.js
+++ src/containers/Pages/Food_page/index.js
@@ -4,7 +4,7 @@ import { Title,Middle_div } from '../../../styles'
 const Page1 = () => (
   <Middle_div>
     <Title>Foods</Title>
-    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Colibri-thalassinus-001-edit.jpg/1024px-Colibri-thalassinus-001-edit.jpg' width='500px' height='400px'/>
+    <img alt='Hummingbird' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Colibri-thalassinus-001-edit.jpg/1024px-Colibri-thalassinus-001-edit.jpg' width='500px' height='400px'/>
     <p>For nutrition, hummingbirds eat a variety of insects, including mosquitoes, fruit flies, gnats in flight, or aphids on leaves and spiders in their webs. The lower beak of hummingbirds is flexible and can bend as much as 25 degrees when it widens at the base, making a larger surface for catching insects. Hummingbirds hover within insect swarms in a method called "hover-hawking" to facilitate feeding.</p>
     <br/>
     <p>To supply energy needs, hummingbirds drink nectar, a sweet liquid inside certain flowers. Like bees, they are able to assess the amount of sugar in the nectar they drink; they normally reject flower types that produce nectar that is less than 10% sugar and prefer those whose sugar content is higher. Nectar is a mixture of glucose, fructose, and sucrose, and is a poor source of other nutrients, requiring hummingbirds to meet their nutritional needs by consuming insects.</p>
